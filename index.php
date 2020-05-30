8c991c3bab3a08b6324eb33bfb3eaf1757dd94f0
Eugeniy <primakua90@gmail.com>
2020-05-30 16:46:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+const TOKEN = '1276287592:AAG8Bll-uyge042VPDdn3h02nnS3vyN35yM';
+const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
+
+function sendRequest($method, $params = []){
+    if (!empty($params)){
+        $url = BASE_URL . $method . '?' . http_build_query($params);
+    }else{
+        $url = BASE_URL . $method;
+    }
+    return  json_decode(
+        file_get_contents($url),
+        JSON_OBJECT_AS_ARRAY);
+}
+
+$updates = sendRequest('getUpdates');
+
+$time = date('Y-m-d H:i:s');
+
+foreach ($updates['result'] as $update){
+    $chat_id = $update['message']['chat']['id'];
+    sendRequest('sendMessage', ['chat_id' => $chat_id, 'text' => $time]);
+}
